f9b99a0b55f2a49130ce14ba2a4788b9bdd9ff4e
Anastasiya-skor <skorokhodova.a.e@yandex.ru>
2022-07-12 05:40:26 +0300

    вычисление расстояния при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/qa/sandbox/MyFirstProgram.java
@@ -8,10 +8,7 @@ public class MyFirstProgram {
         Point p2 = new Point();
         p2.x = 4;
         p2.y = 5;
-        System.out.println("Расстояние между точками " + "p1" + "(" + p1.x + "," + p1.y + ")" + " и " + "p2" + "(" + p2.x + "," + p2.y + ")" + " равно " + distance(p1, p2));
+        System.out.println("Расстояние между точками " + "p1" + "(" + p1.x + "," + p1.y + ")" + " и " + "p2" + "(" + p2.x + "," + p2.y + ")" + " равно " + p1.distance(p2));
 
-    }
-    public static double distance(Point p1, Point p2){
-         return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
 }
--- sandbox/src/main/java/ru/stqa/qa/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/qa/sandbox/Point.java
@@ -3,4 +3,14 @@ package ru.stqa.qa.sandbox;
 public class Point {
     public double x;
     public double y;
+
+
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
+
+
 }
+
+
+
